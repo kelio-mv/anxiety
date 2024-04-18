7d462c21d077454c506678956a4c52b6fec46915
kelio-mv <keliojosue2@gmail.com>
2024-04-18 00:26:05 -0300

    Fix broken className

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -2,7 +2,7 @@ import Icon from "./Icon";
 
 function Page({ title, icon, flex, children }) {
   return (
-    <div className={"space-y-4" + (flex ? "flex flex-col min-h-full" : "")}>
+    <div className={"space-y-4 " + (flex ? "flex flex-col min-h-full" : "")}>
       <div className="space-x-3">
         <Icon name={icon} />
         <span className="text-2xl align-middle">{title}</span>
