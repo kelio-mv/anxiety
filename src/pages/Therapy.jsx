1347bf5222abfe22de29adbfa88750adf55ef1eb
kelio-mv <keliojosue2@gmail.com>
2024-04-18 17:40:18 -0300

    Improved text

--- src/pages/Therapy.jsx
+++ src/pages/Therapy.jsx
@@ -5,7 +5,7 @@ function Therapy() {
     <Page title="Terapia" icon="psychology">
       <div className="max-w-[40rem] space-y-4">
         <p>
-          Todas as técnicas que você conheceu ajudam a aliviar sintomas mas não substituem o
+          Todas as técnicas que você conheceu ajudam a aliviar os sintomas mas não substituem o
           acompanhamento psicológico.
         </p>
         <p>
@@ -13,7 +13,7 @@ function Therapy() {
           transtorno de ansiedade, pois tem se mostrado eficaz em inúmeras pesquisas desenvolvidas.
         </p>
         <p>
-          Ela permite que o paciente identifique e mude padrões de pensamento e comportamento e
+          Ela permite que o paciente identifique e mude padrões de pensamento e de comportamento e
           desenvolva novas interpretações em relação às situações do presente e do futuro, reduzindo
           os sintomas e as crises de ansiedade.
         </p>
