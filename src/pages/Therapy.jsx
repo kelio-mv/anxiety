0c85f53087e769e53fbc0bae43c2fe711a025ba7
kelio-mv <keliojosue2@gmail.com>
2024-04-18 17:36:09 -0300

    Created Therapy page

--- src/pages/Therapy.jsx
+++ src/pages/Therapy.jsx
@@ -1,5 +1,29 @@
+import Page from "../components/Page";
+
 function Therapy() {
-  return <h1>Therapy</h1>;
+  return (
+    <Page title="Terapia" icon="psychology">
+      <div className="max-w-[40rem] space-y-4">
+        <p>
+          Todas as técnicas que você conheceu ajudam a aliviar sintomas mas não substituem o
+          acompanhamento psicológico.
+        </p>
+        <p>
+          A terapia cognitiva-comportamental é o tratamento mais indicado para os casos de
+          transtorno de ansiedade, pois tem se mostrado eficaz em inúmeras pesquisas desenvolvidas.
+        </p>
+        <p>
+          Ela permite que o paciente identifique e mude padrões de pensamento e comportamento e
+          desenvolva novas interpretações em relação às situações do presente e do futuro, reduzindo
+          os sintomas e as crises de ansiedade.
+        </p>
+        <p>
+          Procure um psicólogo que seja especializado nesse tipo de abordagem e agende uma consulta
+          online ou presencial, de acordo com sua preferência e necessidade.
+        </p>
+      </div>
+    </Page>
+  );
 }
 
 export default Therapy;
