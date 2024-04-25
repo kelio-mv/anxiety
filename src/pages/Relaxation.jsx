b9396cbca57d56e62488d363ae948843b00b29d6
kelio-mv <keliojosue2@gmail.com>
2024-04-25 20:15:09 -0300

    Created page Relaxation

--- src/pages/Relaxation.jsx
+++ src/pages/Relaxation.jsx
@@ -1,5 +1,39 @@
+import Page from "../components/Page";
+import Icon from "../components/Icon";
+
 function Relaxation() {
-  return <h1>Relaxation</h1>;
+  return (
+    <Page title="Relaxamento" icon="physical_therapy">
+      <p>
+        A ansiedade provoca a contração dos músculos como forma de defesa, mas acaba gerando mais
+        tensão no corpo, dores e desconforto.
+      </p>
+      <p>
+        A técnica de relaxamento muscular progressivo de Jacobson reduz a tensão muscular através da
+        contração e descontração dos músculos.
+      </p>
+      <p>
+        Assista este vídeo para aprender a fazer essa técnica, ou se preferir, procure outros vídeos
+        no{" "}
+        <a
+          className="text-sky-400 underline text-nowrap"
+          href="https://www.youtube.com/results?search_query=relaxamento+muscular+progressivo+"
+        >
+          YouTube
+        </a>
+      </p>
+      <iframe
+        className="aspect-video"
+        width="100%"
+        src="https://www.youtube.com/embed/aTam6a_twPg?si=HuKDC8IoWRjorU5P"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      />
+    </Page>
+  );
 }
 
 export default Relaxation;
