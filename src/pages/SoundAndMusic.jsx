1fbc82eff665f3f7735088aa00f5ca18b75173f1
kelio-mv <keliojosue2@gmail.com>
2024-04-25 16:25:05 -0300

    Prevent parallel execution and wrap audio name

--- src/pages/SoundAndMusic.jsx
+++ src/pages/SoundAndMusic.jsx
@@ -1,8 +1,19 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 import Page from "../components/Page";
-import Icon from "../components/Icon";
 
 function SoundAndMusic() {
+  const currentAudioRef = useRef();
+
+  function handleAudioPlay(e) {
+    const audio = e.target;
+    const currentAudio = currentAudioRef.current;
+
+    if (currentAudio && currentAudio !== audio) {
+      currentAudio.pause();
+    }
+    currentAudioRef.current = audio;
+  }
+
   return (
     <Page title="Som e Música" icon="headphones">
       <p>
@@ -18,34 +29,37 @@ function SoundAndMusic() {
         artist="Good_B_Music"
         albumCover="https://cdn.pixabay.com/audio/2024/02/14/22-35-32-207_200x200.png"
         src="https://cdn.pixabay.com/download/audio/2024/02/14/audio_b9bc3934cc.mp3?filename=perfect-beauty-191271.mp3"
+        onPlay={handleAudioPlay}
       />
       <Audio
         name="Deep Meditation"
         artist="Grand_Project"
         albumCover="https://cdn.pixabay.com/audio/2024/02/24/20-43-45-882_200x200.jpg"
         src="https://cdn.pixabay.com/download/audio/2024/02/24/audio_1fe2d5f39b.mp3?filename=deep-meditation-192828.mp3"
+        onPlay={handleAudioPlay}
       />
       <Audio
         name="Meditation at the river yoga zen relaxation positive sleep music"
         artist="AlanFrijns"
         albumCover="https://cdn.pixabay.com/audio/2023/02/27/19-57-11-935_200x200.jpg"
         src="https://cdn.pixabay.com/download/audio/2023/02/26/audio_4c182f7459.mp3?filename=meditation-at-the-river-yoga-zen-relaxation-positive-sleep-music-140641.mp3"
+        onPlay={handleAudioPlay}
       />
     </Page>
   );
 }
 
-function Audio({ name, artist, albumCover, src }) {
+function Audio({ name, artist, albumCover, src, onPlay }) {
   return (
     <div className="p-2 space-y-2 border border-gray-800 rounded-lg">
       <div className="flex items-center gap-x-4">
         <img className="w-12 border-2 border-gray-800 rounded" src={albumCover} alt="album cover" />
-        <div>
-          <div>{name}</div>
+        <div className="overflow-hidden">
+          <div className="overflow-hidden text-nowrap text-ellipsis">{name}</div>
           <div className="text-gray-400">{artist}</div>
         </div>
       </div>
-      <audio className="w-full" src={src} controls></audio>
+      <audio className="w-full" src={src} controls onPlay={onPlay}></audio>
     </div>
   );
 }
